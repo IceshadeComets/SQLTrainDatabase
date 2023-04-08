d21919d96e1bbeeda758ed824918fac2ac64be73
Nouman Syed / IceshadeComets <55673795+IceshadeComets@users.noreply.github.com>
2023-04-08 17:46:40 -0600

    Commit 5: Searching Manually

--- SQLTableCode/home.php
+++ SQLTableCode/home.php
@@ -59,155 +59,131 @@ if ($mysqli->connect_errno) {
 </head>
 
 <body bgcolor="FBB917">
-    <h1> Search For Client Information</h1>
+    <h1>Information Lookup</h1>
     <form action='home.php' method="POST">
-        <label for="user">Client Name</label><br>
+        <label for="user">Table Name</label><br>
         <input type='text' name= 'name' id="name" required/> <br> <br>
+        <label for="user">Identifer</label><br>
+        <input type='text' name= 'pk' id="pk" required/> <br> <br>
         <input type='submit' name= 'clientsearch' id="clientsearch" required/> <br> <br>
     </form>
 
     <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["clientsearch"])) {
     // Retrieve the input data to display
     $name = $_POST["name"];
-    $result = mysqli_query($mysqli,"SELECT * FROM client WHERE client.ClientName = '$name'");
-    if($result->num_rows >= 1){
-        echo "<h2>Search Results:</h2>";
-        echo "<table border='1'>
-        <tr>
-        <th>Client Name</th>
-        <th>Branch ID</th>
-        </tr>";
-        while($row = mysqli_fetch_array($result))
-        {
-            echo "<tr>";
-            echo "<td>" . $row['ClientName'] . "</td>";
-            echo "<td>" . $row['BranchID'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    }
-    else{
-        //echo "<h2>No results found.</h2>";
-    }
-}
-         ?>
+    $pkey = $_POST["pk"];
 
-<h1> Search For Employee Information</h1> 
-    <form action='home.php' method="POST">
-        <label for="user">Employee Name/SSN</label><br>
-        <input type='text' name= 'name' id="name" required/> <br> <br>
-        <input type='submit' name= 'empsearch' id="empsearch" required/> <br> <br>
-    </form>
-
-    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["empsearch"])) {
-    // Note for this crap, we will need to later sort this out by user type, etc User is Supervisor/CEo
-    // Retrieve the input data to display
-    $name = $_POST["name"];
-    $result = mysqli_query($mysqli,"SELECT * FROM employee as E WHERE E.firstname = '$name'");
-    $sresult = mysqli_query($mysqli,"SELECT * FROM employee as E WHERE E.ssn = '$name'");
-    if($result->num_rows >= 1){
-        echo "<h2>Search Results:</h2>";
-        echo "<table border='1'>
-        <tr>
-        <th>SSN</th>
-        <th>CEOSSN</th>
-        <th>Address</th>
-        <th>FirstName</th>
-        <th>MiddleName</th>
-        <th>LastName</th>
-        <th>Birthdate</th>
-        <th>Salary</th>
-        <th>Sex</th>
-        </tr>";
-        while($row = mysqli_fetch_array($result))
-        {
+    $xKey = mysqli_query($mysqli, "SELECT COLUMN_NAME FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_NAME = '$name' AND CONSTRAINT_NAME = 'PRIMARY'");
+    if ($xKey) {
+        $primaryKey = mysqli_fetch_assoc($xKey);
+        $primaryKeyColumn = $primaryKey['COLUMN_NAME'];
+        echo "The primary key column for $name is: $primaryKeyColumn";
+        $result = mysqli_query($mysqli,"SELECT * FROM $name WHERE $name.$primaryKeyColumn = '$pkey'");
+        if($result->num_rows >= 1){
+            //$result->num_rows;
+            echo "<h2>Search Results:</h2>";
+            echo "<table border='1'>";
+            
+            // Print table headers
+            $columnsResult = mysqli_query($mysqli, "SHOW COLUMNS FROM $name");
+            $columns = mysqli_fetch_all($columnsResult, MYSQLI_ASSOC);
             echo "<tr>";
-            echo "<td>" . $row['SSN'] . "</td>";
-            echo "<td>" . $row['CEOSSN'] . "</td>";
-            echo "<td>" . $row['Address'] . "</td>";
-            echo "<td>" . $row['FirstName'] . "</td>";
-            echo "<td>" . $row['MiddleName'] . "</td>";
-            echo "<td>" . $row['LastName'] . "</td>";
-            echo "<td>" . $row['Birthdate'] . "</td>";
-            echo "<td>" . $row['Salary'] . "</td>";
-            echo "<td>" . $row['Sex'] . "</td>";
+            foreach ($columns as $column) {
+                echo "<th>" . $column['Field'] . "</th>";
+            }
             echo "</tr>";
+    
+            // Print table rows
+            while($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                foreach ($columns as $column) {
+                    echo "<td>" . $row[$column['Field']] . "</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "<h2>No results found.</h2>";
         }
-        echo "</table>";
-    } else if($sresult->num_rows >= 1){
-        echo "<h2>Search Results:</h2>";
-        echo "<table border='1'>
-        <tr>
-        <th>SSN</th>
-        <th>CEOSSN</th>
-        <th>Address</th>
-        <th>FirstName</th>
-        <th>MiddleName</th>
-        <th>LastName</th>
-        <th>Birthdate</th>
-        <th>Salary</th>
-        <th>Sex</th>
-        </tr>";
-        while($row = mysqli_fetch_array($sresult))
-        {
-            echo "<tr>";
-            echo "<td>" . $row['SSN'] . "</td>";
-            echo "<td>" . $row['CEOSSN'] . "</td>";
-            echo "<td>" . $row['Address'] . "</td>";
-            echo "<td>" . $row['FirstName'] . "</td>";
-            echo "<td>" . $row['MiddleName'] . "</td>";
-            echo "<td>" . $row['LastName'] . "</td>";
-            echo "<td>" . $row['Birthdate'] . "</td>";
-            echo "<td>" . $row['Salary'] . "</td>";
-            echo "<td>" . $row['Sex'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    } 
-    else{
-        //echo "<h2>No results found.</h2>";
     }
 }
          ?>
 
-<h1> Search For Train Specification</h1> 
-    <form action='home.php' method="POST">
-        <label for="user">TrainID</label><br>
-        <input type='text' name= 'name' id="name" required/> <br> <br>
-        <input type='submit' name= 'tsearch' id="tsearch" required/> <br> <br>
-    </form>
+<?php
+// If form is submitted, insert data into table
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["table_name"])) {
+    $table_name = $_POST["table_name"];
 
-    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["tsearch"])) {
-    // Note for this crap, we will need to later sort this out by user type, etc User is Supervisor/CEo
-    // Retrieve the input data to display
-    $name = $_POST["name"];
-    $result = mysqli_query($mysqli,"SELECT * FROM train as T WHERE T.trainid = '$name'");
-    if($result->num_rows >= 1){
-        echo "<h2>Search Results:</h2>";
-        echo "<table border='1'>
-        <tr>
-        <th>TrainID</th>
-        <th>InspectorSSN</th>
-        <th>BranchID</th>
-        <th>LocomotiveType</th>
-        </tr>";
-        while($row = mysqli_fetch_array($result))
-        {
-            echo "<tr>";
-            echo "<td>" . $row['TrainID'] . "</td>";
-            echo "<td>" . $row['InspectorSSN'] . "</td>";
-            echo "<td>" . $row['BranchID'] . "</td>";
-            echo "<td>" . $row['LocomotiveType'] . "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
-    } 
-    else{
-        //echo "<h2>No results found.</h2>";
+    // Get fields for specified table
+    $fields_query = "SHOW COLUMNS FROM $table_name";
+    $fields_result = mysqli_query($mysqli, $fields_query);
+
+    // Construct list of field names for SQL query
+    $field_names = [];
+    while ($field_row = mysqli_fetch_assoc($fields_result)) {
+        $field_names[] = $field_row['Field'];
+    }
+    $field_names_str = implode(", ", $field_names);
+
+    // Construct list of escaped field values for SQL query
+    $field_values = [];
+    foreach ($field_names as $field_name) {
+        $field_value = mysqli_real_escape_string($mysqli, $_POST[$field_name]);
+        $field_values[] = "'$field_value'";
+    }
+    $field_values_str = implode(", ", $field_values);
+
+    // Construct and execute SQL query
+    $query = "INSERT INTO $table_name ($field_names_str) VALUES ($field_values_str)";
+    $result = mysqli_query($mysqli, $query);
+
+    // Check for errors
+    if (!$result) {
+        echo "Error: " . mysqli_error($mysqli);
+    } else {
+        echo "Entry added successfully!";
     }
 }
-         ?>
 
+// If no table is specified, show dropdown to select table
+if (!isset($_POST["table_name"])) {
+    $tables_query = "SHOW TABLES";
+    $tables_result = mysqli_query($mysqli, $tables_query);
+
+    if (!$tables_result) {
+        echo "Error: " . mysqli_error($mysqli);
+    } else {
+        echo "<form method='POST'>";
+        echo "<label for='table_name'>Table name:</label>";
+        echo "<select name='table_name' id='table_name'>";
+        while ($table_row = mysqli_fetch_row($tables_result)) {
+            echo "<option value='$table_row[0]'>$table_row[0]</option>";
+        }
+        echo "</select>";
+        echo "<br><br>";
+        echo "<input type='submit' value='Select table'>";
+        echo "</form>";
+    }
+} else { // If table is specified, show form with input fields for each table column
+    $table_name = $_POST["table_name"];
+
+    $fields_query = "SHOW COLUMNS FROM $table_name";
+    $fields_result = mysqli_query($mysqli, $fields_query);
+
+    if (!$fields_result) {
+        echo "Error: " . mysqli_error($mysqli);
+    } else {
+        echo "<form method='POST'>";
+        while ($field_row = mysqli_fetch_assoc($fields_result)) {
+            $field_name = $field_row['Field'];
+            echo "<label for='$field_name'>$field_name:</label>";
+            echo "<input type='text' name='$field_name' id='$field_name'><br><br>";
+        }
+        echo "<input type='submit' value='Add entry'>";
+        echo "</form>";
+    }
+}
+?>
 
 </body>
 </html>
